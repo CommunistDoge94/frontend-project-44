8078fc26e6900ea7e51d999058a4563bc789dcf7
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-01-27 00:36:45 +0300

    - Добавление программы "brain-even".

---
+++ bin/brain-even.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import askName from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = askName();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let round = 0; round < 3; round += 1) {
+  const number = Math.floor(Math.random() * 98) + 2; // Диапазон от 2 до 99.
+  const correctAnswer = (number % 2 === 0) ? 'yes' : 'no';
+
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: '); 
+
+  if (correctAnswer === answer) {
+    console.log('Correct!');
+    if (round === 2) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+    break;
+  }
+}
