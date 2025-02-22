1a93419d7075c2913d0f288be6cbcadc25d4f237
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-02-22 23:34:23 +0300

    1. Улучшение связанные с 'round' и переворот условия 'userAnswer === correctAnswer'. 2. Удаление импорта cli.js и перенос функций в index.js.

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,10 @@
 import readlineSync from 'readline-sync';
 
-function greetUser() {
+const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 }
 
-function askQuestion(question) {
-  console.log(`Question: ${question}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  return userAnswer;
-}
-
-export { greetUser, askQuestion };
+export { greetUser };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
-import { greetUser, askQuestion } from './cli.js';
+import readlineSync from 'readline-sync';
 
-function startGame(gameDescription, generateQuestion) {
+const askQuestion = (question) => {
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  return userAnswer;
+}
+
+const greetUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+}
+
+const startGame = (gameDescription, generateQuestion) => {
   const userName = greetUser();
   const roundCount = 3;
   console.log(gameDescription);
 
-  for (let round = 0; round < roundCount; round += 1) {
+  for (let round = 1; round <= roundCount; round += 1) {
     const [question, correctAnswer] = generateQuestion();
     const userAnswer = askQuestion(question);
 
     if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-      if (round === 2) {
-        console.log(`Congratulations, ${userName}!`);
+      if (userAnswer !== correctAnswer) {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+        console.log(`Let's try again, ${userName}!`);
+        break;
       }
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      break;
+
+    console.log('Correct!');
+    if (round === 3) {
+      console.log(`Congratulations, ${userName}!`);
+  }
     }
   }
 }
