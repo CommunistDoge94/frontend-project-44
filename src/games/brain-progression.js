0ec417d421f924b34fbf755aca37e792bb32dcc2
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-02-23 18:25:33 +0300

    1. Трансформация 'generateProgression' в чистую функцию. 2. Генерация чисел через 'generateNumber'.

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,24 +1,29 @@
-import startGame from '../index.js';
+import { startGame, generateNumber } from '../index.js';
 
-const generateProgression = () => {
+const generateProgression = (progressionLength, startNum, numGap, emptyPlace) => {
   const progression = [];
-  const progressionLength = Math.floor(Math.random() * (12 - 8 + 1)) + 8;
-  const numGap = Math.floor(Math.random() * 15 + 1);
-  const startNum = Math.floor(Math.random() * 10 + 1);
   for (let progressionNumber = 0; progressionNumber < progressionLength; progressionNumber += 1) {
     if (progressionNumber === 0) {
       progression.push(startNum);
     }
     progression.push(startNum + (numGap * (progressionNumber + 1)));
   }
-  const emptyPlace = Math.floor(Math.random() * progressionLength);
   const correctAnswer = String(progression[emptyPlace]);
   progression[emptyPlace] = '..';
   return [progression.join(' '), correctAnswer];
 };
 
 const generateQuestion = () => {
-  const [question, correctAnswer] = generateProgression();
+  const progressionLength = Math.floor(Math.random() * (12 - 8 + 1)) + 8;
+  const numGap = generateNumber();
+  const startNum = generateNumber();
+  const emptyPlace = Math.floor(Math.random() * progressionLength);
+  const [question, correctAnswer] = generateProgression(
+    progressionLength,
+    numGap,
+    startNum,
+    emptyPlace,
+  );
   return [question, correctAnswer];
 };
 
