67fb611793035449583e219675682779a5a4e8a7
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-02-24 19:30:57 +0300

    Изменение 'generateProgression', теперь правильный ответ и замена в прогрессии происходят внутри 'generateQuestion'.

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,7 +1,7 @@
 import startGame from '../index.js';
 import generateNumber from '../utils.js';
 
-const generateProgression = (progressionLength, startNum, numGap, emptyPlace) => {
+const generateProgression = (progressionLength, startNum, numGap) => {
   const progression = [];
   for (let progressionNumber = 0; progressionNumber < progressionLength; progressionNumber += 1) {
     if (progressionNumber === 0) {
@@ -9,9 +9,7 @@ const generateProgression = (progressionLength, startNum, numGap, emptyPlace) =>
     }
     progression.push(startNum + (numGap * (progressionNumber + 1)));
   }
-  const correctAnswer = String(progression[emptyPlace]);
-  progression[emptyPlace] = '..';
-  return [progression.join(' '), correctAnswer];
+  return progression;
 };
 
 const generateQuestion = () => {
@@ -19,13 +17,18 @@ const generateQuestion = () => {
   const numGap = generateNumber();
   const startNum = generateNumber();
   const emptyPlace = generateNumber(progressionLength);
-  const [question, correctAnswer] = generateProgression(
+  
+  const progression = generateProgression(
     progressionLength,
     numGap,
     startNum,
     emptyPlace,
   );
-  return [question, correctAnswer];
+  
+  const correctAnswer = String(progression[emptyPlace]);
+  progression[emptyPlace] = '..';
+
+  return [progression.join(' '), correctAnswer];
 };
 
 const gameDescription = 'What number is missing in the progression?';
